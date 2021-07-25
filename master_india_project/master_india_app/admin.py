3bbd2d9032f82c4db0d4cbcbb9f208837c9632c7
bikash-pal <bpal13577@gmail.com>
2021-07-26 00:59:13 +0530

    sttings app and restapi added to app models created without relation serializers.py created migration done super user created model is working fine

--- master_india_project/master_india_app/admin.py
+++ master_india_project/master_india_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categories,SubCatagories,Products
+admin.site.register(Categories)
+admin.site.register(SubCatagories)
+admin.site.register(Products)
 
 # Register your models here.
--- master_india_project/master_india_app/models.py
+++ master_india_project/master_india_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    catagorie=models.CharField(max_length=100)
+class SubCatagories(models.Model):
+    #question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    subCatagories=models.CharField(max_length=100)
+class Products(models.Model):
+    #question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    products=models.CharField(max_length=100)
+
+
+
+
